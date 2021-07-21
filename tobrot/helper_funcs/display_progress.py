0889e9957233bfad6f4a4ebdcfc6f89194f24f18
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-21 18:29:46 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -77,18 +77,25 @@ class Progress:
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = "<b>╭────── ⌊__UploadinG: 〘{2}%〙 📤__⌉</b>\n│ \n<b>├〖{0}{1}〗</b>\n".format(
-                ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
-                ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
-                round(percentage, 2))
-            #cpu = "{psutil.cpu_percent()}%"
-            tmp = progress +"│" + "\n**├Total 📀:**   〘<code>{1}</code>〙\n**├Done ✓ :** 〘<code>{0}</code>〙\n**├Speed** 🚀 :  〘<code>{2}/s</code>〙\n**├ETA** ⏳ :  〘<code>{3}</code>〙\n**│**\n**╰── ⌊ Thank You ⌉**".format(
+            progress = "[{0}{1}] \nP: {2}%\n".format(
+                "".join(
+                    [FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]
+                ),
+                "".join(
+                    [
+                        UN_FINISHED_PROGRESS_STR
+                        for i in range(20 - math.floor(percentage / 5))
+                    ]
+                ),
+                round(percentage, 2),
+            )
+
+            tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
                 # elapsed_time if elapsed_time != '' else "0 s",
                 estimated_total_time if estimated_total_time != "" else "0 s",
-            #tmp += "\n│"+"\n╰── ⌊ @TGFilmZone ⌉"
             )
             try:
                 if not self._mess.photo:
