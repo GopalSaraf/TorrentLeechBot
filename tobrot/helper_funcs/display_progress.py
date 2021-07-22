7508d2ccd11e837a83a7720f52c5fa331555f2c7
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-22 20:02:01 +0530

    Update display_progress.py

--- tobrot/helper_funcs/display_progress.py
+++ tobrot/helper_funcs/display_progress.py
@@ -77,7 +77,8 @@ class Progress:
             elapsed_time = TimeFormatter(milliseconds=elapsed_time)
             estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-            progress = f"File: {os.path.basename(local_file_name)}\n\nUploading: {2}% " + "[{0}{1}] \n".format(
+#            progress = f"File: {os.path.basename(local_file_name)}\n\n" + "Uploading: {2}% \n[{0}{1}] \n".format(
+            progress = "**Uploading: {2}%** \n[{0}{1}] \n".format(
                 "".join(
                     [FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]
                 ),
@@ -90,7 +91,7 @@ class Progress:
                 round(percentage, 2),
             )
 
-            tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+            tmp = progress + "{0} of {1}\nSpeed: {2}/sec\nETA: {3}\n".format(
                 humanbytes(current),
                 humanbytes(total),
                 humanbytes(speed),
