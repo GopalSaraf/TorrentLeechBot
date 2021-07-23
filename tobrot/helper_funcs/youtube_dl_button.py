f392cde3f8d808571a422fef1bbdc334cfdb4073
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-23 19:39:18 +0530

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -79,7 +79,7 @@ async def youtube_dl_call_back(bot, update):
     # https://superuser.com/a/994060
     LOGGER.info(custom_file_name)
     #
-    await update.message.edit_caption(caption="**DownloadinG.... please wait..**")
+    await update.message.edit_caption(caption="**Downloading.... please wait..**")
 
     tmp_directory_for_each_user = os.path.join(
         DOWNLOAD_LOCATION, str(update.message.message_id)
