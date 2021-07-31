4ec89434a97a00da89717419e22e38f09d31d740
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-07-31 12:25:13 +0530

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -348,7 +348,7 @@ async def upload_single_file(
             message_for_progress_display = message
             if not edit_media:
                 message_for_progress_display = await message.reply_text(
-                    "Trying to upload <code>{}</code>".format(os.path.basename(local_file_name))
+                    "Starting upload of {}".format(os.path.basename(local_file_name))
                 )
                 prog = Progress(from_user, client, message_for_progress_display, os.path.basename(local_file_name))
             if local_file_name.upper().endswith(("MKV", "MP4", "WEBM", "M4V", "3GP")):
