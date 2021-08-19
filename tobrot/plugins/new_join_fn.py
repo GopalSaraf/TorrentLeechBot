ea711dceb14e18bb5c55fd33f8c50fcc42161473
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-08-19 10:47:37 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -21,7 +21,7 @@ async def new_join_f(client, message):
                )
             )
         # leave chat
-        await client.leave_chat(chat_id=message.chat.id, delete=True)
+        # await client.leave_chat(chat_id=message.chat.id, delete=True)
     # delete all other messages, except for AUTH_CHANNEL
     await message.delete(revoke=True)
 
