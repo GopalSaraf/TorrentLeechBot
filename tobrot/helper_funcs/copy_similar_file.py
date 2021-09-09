ef9f80684594f3cf244261e67a236e4a484e2051
Gopal Saraf <83419951+GopalSaraf@users.noreply.github.com>
2021-09-09 12:46:32 +0530

    Update copy_similar_file.py

--- tobrot/helper_funcs/copy_similar_file.py
+++ tobrot/helper_funcs/copy_similar_file.py
@@ -2,7 +2,7 @@ import os
 import time
 from shutil import copyfile
 
-
+# idk
 async def copy_file(input_file, output_dir):
     output_file = os.path.join(output_dir, str(time.time()) + ".jpg")
     # https://stackoverflow.com/a/123212/4723940
