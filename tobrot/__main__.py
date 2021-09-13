593f2b4cac8a50f7e2066b4f923384196bec3e71
GopalSaraf <sarafgopal37@gmail.com>
2021-09-13 20:12:46 +0530

    Minor Changes

--- tobrot/__main__.py
+++ tobrot/__main__.py
@@ -33,7 +33,8 @@ from tobrot import (
     TOGGLE_VID,
     TOGGLE_DOC,
     HELP_COMMAND,
-    LIST_COMMAND
+    LIST_COMMAND,
+    LIST_COMMAND_2,
 )
 from tobrot.helper_funcs.download import down_load_media_f
 from tobrot.plugins.call_back_button_handler import button
@@ -226,14 +227,14 @@ if __name__ == "__main__":
     #
     help_message_handler = MessageHandler(
         help_message_f,
-        filters=filters.command([f"{HELP_COMMAND}"])
+        filters=filters.command([HELP_COMMAND])
                 & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(help_message_handler)
     #
     list_message_handler = MessageHandler(
         list_fn,
-        filters=filters.command([f"{LIST_COMMAND}"])
+        filters=filters.command([LIST_COMMAND, LIST_COMMAND_2])
                 & filters.chat(chats=AUTH_CHANNEL),
     )
     app.add_handler(list_message_handler)
